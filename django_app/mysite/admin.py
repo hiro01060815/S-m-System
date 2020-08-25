11effb50c56489824d9553dd80abf1556e57d63b
hiro01060815 <al18009@shibaura-it.ac.jp>
2020-08-25 12:11:03 +0900

    履修科目のモデルを作成2

--- django_app/mysite/admin.py
+++ django_app/mysite/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from mysite.models import CoursesTaken,Post
+from mysite.models import CoursesTaken,Post,UserCTInfo
 
 admin.site.register(CoursesTaken)
 admin.site.register(Post)
+admin.site.register(UserCTInfo)
